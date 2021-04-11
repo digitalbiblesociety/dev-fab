06f73833c6602c96e66951b0733c06dbe0c5e585
Jon Bitgood <jon@dbs.org>
2021-04-10 22:25:51 -0500

    Removed Subfooter Section

--- resources/views/countries/show.blade.php
+++ resources/views/countries/show.blade.php
@@ -1,25 +1,7 @@
 @extends('_layouts.main')
 
-@section('subheader')
-<style>
-    #languages tr td:nth-child(-n+3),
-    #languages tr th:nth-child(-n+3) {
-        display: none;
-    }
-</style>
-@endsection
-
 @section('main')
 
-    @include('shin::countries.show')
+    @include('shin::countries.show', ['country' => $country])
 
-@endsection
-
-
-@section('subfooter')
-    <script
-            src="https://code.jquery.com/jquery-2.2.4.min.js"
-            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-            crossorigin="anonymous"></script>
-    <script src="{{ mix('js/dataTables.js') }}"></script>
 @endsection
