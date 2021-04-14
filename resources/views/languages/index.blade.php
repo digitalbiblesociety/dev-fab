c1bf69237b99f4c5b97b84d065a9a7082e18dcfe
Ken Bitgood <ken@dbs.org>
2021-04-14 09:09:20 -0500

    Langauge Page Styling

--- resources/views/languages/index.blade.php
+++ resources/views/languages/index.blade.php
@@ -1,3 +1,13 @@
+@section('header')
+    @parent
+    <style>
+        .table i {background-color: var(--primary-color);width: 2.7em; color: #fff;
+                    border-radius: 16px; margin: 0 auto; font-size:.8em; padding:3px;}
+    </style>
+
+@endsection
+
+
 @extends('_layouts.main')
 
 @section('main')
