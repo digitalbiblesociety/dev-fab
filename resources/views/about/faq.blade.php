20e46570198b7e7c1efb77162f580d2af2830f8b
Ken Bitgood <ken@dbs.org>
2021-04-17 16:43:20 -0500

    FAQ Updated Page, Styles and Translations

--- resources/views/about/faq.blade.php
+++ resources/views/about/faq.blade.php
@@ -3,13 +3,14 @@
 @section('header')
     @parent
     <title>{{ trans('shin::fields.faq') }} | {{ trans('app.title') }}</title>
+    <style>
+        #faq            {padding:2em 10%;}
+        #faq div        {margin-top:1em;}
+        #faq h3, #faq p {padding:1rem; text-align: left;}
+        .faq-icon       {}
+    </style>
 @endsection
 
-@section('subheader')
-<style>
-    #faq h3, #faq p {padding:1rem;}
-</style>
-@endsection
 
 @section('subnav')
 
@@ -27,22 +28,49 @@
 
 
     <div class="row small-center" id="faq">
-        <div class="small-10 medium-10">
-        <h3>{{ trans('app.about.faq.1_title') }}</h3>
-        <p>{{ trans('app.about.faq.1_description') }}</p>
-        <h3>{{ trans('app.about.faq.2_title') }}</h3>
-        <p>{{ trans('app.about.faq.2_description') }}</p>
-        <h3>{{ trans('app.about.faq.3_title') }}</h3>
-        <p>{{ trans('app.about.faq.3_description') }}</p>
-        <h3>{{ trans('app.about.faq.4_title') }}</h3>
-        <p>{{ trans('app.about.faq.4_description') }}</p>
-        <h3>{{ trans('app.about.faq.5_title') }}</h3>
-        <p>{{ trans('app.about.faq.5_description') }}</p>
-        <h3>{{ trans('app.about.faq.6_title') }}</h3>
-        <p>{{ trans('app.about.faq.6_description') }}</p>
-        <h3>{{ trans('app.about.faq.7_title') }}</h3>
-        <p>{{ trans('app.about.faq.7_description') }}</p>
-    </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/icon_fab_notitle.svg" alt="About Find.Bible" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.1_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.1_description') }}</p>
+        </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/fab_color_resources.svg" alt="Find.Bible Creation" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.2_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.2_description') }}</p>
+        </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/fab_color_forumn.svg" alt="Find.Bible Forumn" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.3_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.3_description') }}</p>
+        </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/fab_color_group.svg" alt="Who is FOBAI" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.4_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.4_description') }}</p>
+        </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/fab_color_faq3.svg" alt="Find.Bible Ownership" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.5_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.5_description') }}</p>
+        </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/fab_color_team.svg" alt="Find.Bible Team" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.6_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.6_description') }}</p>
+        </div>
+
+        <div class="row">
+            <div class="small-1"><img class="faq-icon" src="/img/fab_color_helping.svg" alt="Find.Bible Work" /></div>
+            <div class="small-11"><h3>{{ trans('app.about.faq.7_title') }}</h3></div>
+            <p>{{ trans('app.about.faq.7_description') }}</p>
+        </div>
+
     </div>
 
 @endsection
