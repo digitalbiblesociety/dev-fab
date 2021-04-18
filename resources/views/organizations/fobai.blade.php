487a6ed4afc17fe25d2e6343dc9fd2283dac3f8a
Ken Bitgood <ken@dbs.org>
2021-04-18 12:16:54 -0500

    Agencies Pages Styling

--- resources/views/organizations/fobai.blade.php
+++ resources/views/organizations/fobai.blade.php
@@ -4,14 +4,15 @@
     @parent
     <title>{{ trans('shin::fields.fobai') }} | {{ trans('app.title') }}</title>
     <style>
-        .orgs a {flex-basis:20%; max-height:132px;}
+        .orgs a {flex-basis:20%; height:120px;}
         /* Media Queries 960px 720px 480px */
             @media screen and (max-width:60em){ .orgs a {flex-basis:25%}}
             @media screen and (max-width:45em){ .orgs a {flex-basis:33%}}
             @media screen and (max-width:30em){ .orgs a {flex-basis:50%}}
 
         .orgs svg {width: 1em;height: 1em;vertical-align: -.15em;fill: currentColor;overflow: hidden;
-                    margin: 0 auto;display: block;font-size: 9rem;}
+                    margin: 0 auto;display: block;font-size: 9rem; max-height:90px;}
+         div.page-title {margin:2em auto 1em auto;}
     </style>
 @endsection
 @section('subnav')
@@ -23,7 +24,15 @@
     ])
 @endsection
 @section('main')
-
+    <section>
+        <div class="row">
+            <div class="row page-title">
+                <svg class="dt-aside-icon" >
+                    <use xmlns:xlink="https://www.w3.org/1999/xlink" xlink:href="/img/icons.svg#people_agencies"></use>
+                </svg>
+                <span class="dt-aside-title">{{ trans('shin::fields.fobai') }}</span>
+            </div>
+        </div>
 
     <div class="row orgs">
         @foreach($organizations as $org)
@@ -31,4 +40,5 @@
         @endforeach
     </div>
 
+    </section>
 @endsection
--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -3,63 +3,26 @@
 @section('header')
     @parent
     <title>{{ trans('shin::fields.agencies') }} | {{ trans('app.title') }}</title>
+
+    <style>
+
+        .dataTables_header  {margin:0 auto; width:33%; min-width:300px;padding:0;}
+        .datTables_length   {width:25%}
+        .dataTables_filter   {width:75%;}
+        thead tr, tr th {display: none;}
+        tr,td {display: block;}
+        tr td:nth-child(2) {display: none;}
+        tr {width:33%;margin:0;height:60px;float:left; border: thin solid #e2e2e2; padding: 12px 0 0 0}
+        td a {display: block;text-align: center;overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-line-clamp: 3;-webkit-box-orient: vertical;}
+
+
+
+        div.page-title {margin:1.8em auto 0;}
+    </style>
 @endsection
 
 @section('subheader')
-    <style>
-        .dataTables_header {
-            width: 50%;
-            height: 40px;
-            margin: 20px auto;
-        }
 
-        .dataTable thead,
-        .dataTable tr.even {
-            background-color: #fff;
-        }
-
-        tr th {
-            display: none;
-        }
-
-        tr,td {
-            display: block;
-        }
-
-        tr td:nth-child(2) {
-            display: none;
-        }
-
-        tr {
-            width:31.5%;
-            margin:0 .5%;
-            height:40px;
-            float:left;
-        }
-
-        td a {
-            display: block;
-            text-align: center;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            display: -webkit-box;
-            -webkit-line-clamp: 3;
-            -webkit-box-orient: vertical;
-        }
-
-        fieldset.filters {
-            margin-top:2rem;
-        }
-
-        #organizations_filter {
-            width: 85%;
-        }
-
-        #organizations_filter input {
-            width: 100%;
-        }
-
-    </style>
 @endsection
 
 
@@ -75,5 +38,14 @@
 @endsection
 
 @section('main')
+    <div class="row">
+        <div class="row page-title">
+            <svg class="dt-aside-icon">
+                <use xmlns:xlink="https://www.w3.org/1999/xlink" xlink:href="/img/icons.svg#people_agencies"></use>
+            </svg>
+            <span class="dt-aside-title">{{ trans('shin::fields.agencies') }}</span>
+        </div>
+    </div>
+
     @include('shin::organizations.index')
 @endsection
