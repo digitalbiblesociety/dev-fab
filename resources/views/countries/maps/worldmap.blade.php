22a0bd9355ec3e6e1d3b461c352b8fe24cd80754
Ken Bitgood <ken@dbs.org>
2021-05-04 11:50:48 -0500

    Update worldmap.blade.php

--- resources/views/countries/maps/worldmap.blade.php
+++ resources/views/countries/maps/worldmap.blade.php
@@ -4,6 +4,9 @@
     @parent
     <style>
         a.nav-countries    {color: var(--primary-color)!important}
+        nav[role=tablist] a {font-size: clamp(.8rem, 3vw, .9rem); padding: 0.7rem .75rem;}
+        .map-view h1        {font-size: clamp(1.2rem, 3vw, 1.6rem); text-align:center;margin-bottom: 1.5rem;}
+        .map-view .row      {margin: 0 10%}
     </style>
 @endsection
 
@@ -16,8 +19,13 @@
         'iconClass'       => 'banner-icon',
         'iconType'        => 'icons',
         'tabs'            => [
-            'world'     => 'world',
-            'africa'    => 'africa'
+            'world'     => trans('shin::fields.world'),
+            'africa'    => trans('shin::fab.index.africa'),
+            'americas'     => trans('shin::fab.index.americas'),
+            'asia'     => trans('shin::fab.index.asia'),
+            'europe'     => trans('shin::fab.index.europe'),
+            'oceania'     => trans('shin::fab.index.oceania'),
+
         ],
         'backgroundImage' => 'https://images.bible.cloud/fab/banners/countries.jpg',
         'breadcrumbs' => [
