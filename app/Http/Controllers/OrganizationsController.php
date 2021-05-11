ddb36feca6d0b7c1690e6af2a61fe44ce77c3cbc
Jon Bitgood <jon@dbs.org>
2021-05-11 10:45:05 -0500

    Added Fractal Transformation for Organization Resources

--- app/Http/Controllers/OrganizationsController.php
+++ app/Http/Controllers/OrganizationsController.php
@@ -8,8 +8,13 @@ use DigitalBibleSociety\Shin\Models\Bible\BibleEquivalent;
 use DigitalBibleSociety\Shin\Models\Organization\Organization;
 use DigitalBibleSociety\Shin\Models\Resource\Film;
 use DigitalBibleSociety\Shin\Models\Resource\Resource;
+use DigitalBibleSociety\Shin\Transformers\BibleTableTransformer;
+use DigitalBibleSociety\Shin\Transformers\ResourcesTableTransformer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use League\Fractal\Serializer\DataArraySerializer;
+use Spatie\Fractal\Fractal;
+use Spatie\Fractalistic\ArraySerializer;
 
 class OrganizationsController extends Controller
 {
@@ -32,7 +37,6 @@ class OrganizationsController extends Controller
     public function show($id)
     {
         $organization = Organization::find($id);
-
         $bible_equivalents = BibleEquivalent::where('bible_equivalents.organization_id', $id)
                                                    ->select('bible_id')->get()->pluck('bible_id')->toArray();
         $bible_organization = BibleOrganization::where('bible_organization.organization_id', $id)
@@ -40,12 +44,12 @@ class OrganizationsController extends Controller
         $bible_links = DB::connection('shin')->table('bible_links')->where('bible_links.organization_id', $id)
             ->select('bible_id')->get()->pluck('bible_id')->toArray();
         $bibles = Bible::whereIn('id',array_merge($bible_equivalents,$bible_organization,$bible_links))->with('language:iso,name','country')->get();
+        $bibles = collect(Fractal::create($bibles, new BibleTableTransformer(), DataArraySerializer::class)->toArray());
 
-        $resources = Resource::where('organization_id',$id)->with('language:iso,name')
-                             ->select('title', 'title_vernacular','iso','link','type')->get()->toArray();
-        $films = Film::where('organization_id',$id)->with('language:iso,name')->select('title', 'title_vernacular','iso','url as link','type as film')
-                                                   ->get()->toArray();
+        $resources = Resource::where('organization_id',$id)->with('language:iso,name')->select('title', 'title_vernacular','iso','link','type')->get()->toArray();
+        $films = Film::where('organization_id',$id)->with('language:iso,name')->select('title', 'title_vernacular','iso','url as link','type as film')->get()->toArray();
         $resources = array_merge($resources,$films);
+        $resources = Fractal::create($resources, new ResourcesTableTransformer(), DataArraySerializer::class)->toArray();
 
         return view('organizations.show', compact('organization', 'bibles','resources'));
     }
