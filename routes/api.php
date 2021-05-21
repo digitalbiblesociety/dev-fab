34f8d8ac643c0026eb91bc07bb659ba0fcf40c5e
Jon Bitgood <jon@dbs.org>
2021-05-21 12:22:03 -0500

    Updated API routes for find.bible

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountriesController;
+use App\Http\Controllers\LanguagesController;
+use App\Http\Controllers\BiblesController;
 
+Route::get('/countries/{id}',             [CountriesController::class, 'showAPI']);
+Route::get('/countries/',                 [CountriesController::class, 'indexAPI']);
+Route::get('/languages/{iso}/resources',  [LanguagesController::class, 'showResourcesAPI']);
+Route::get('/languages/{iso}/bibles',     [LanguagesController::class, 'showBiblesAPI']);
+Route::get('/languages/{iso}',            [LanguagesController::class, 'showAPI']);
+Route::get('/languages/',                 [LanguagesController::class, 'indexAPI']);
 
-Route::get('countries/{id}', [\App\Http\Controllers\CountriesController::class, 'showAPI']);
+Route::get('/bibles/{id}',                [BiblesController::class, 'showAPI']);
+Route::get('/bibles/',                    [BiblesController::class, 'indexAPI']);
+
+//showBiblesAPI
