5b075f294eb06629bb1fc9b069514e51cf011770
Ken Bitgood <ken@dbs.org>
2021-06-04 11:51:34 -0500

    Bible Show Update

--- resources/views/bibles/show.blade.php
+++ resources/views/bibles/show.blade.php
@@ -3,6 +3,7 @@
     @parent
     <style>
         a.nav-bibles  {color: var(--primary-color)!important}
+        #banner        {height:110px}
     </style>
 @endsection
 
@@ -11,15 +12,11 @@
     'title'     => $bible->title,
     'subtitle'  => $bible->title_vernacular,
     'backgroundImage' => 'https://images.bible.cloud/fab/banners/language/'.substr($bible->id,0,3).'.jpg',
-    'tabs' => [
-        i18n_link('bibles') => trans('shin::fields.bibles'),
-        i18n_link('films')  => trans('shin::fields.films'),
-        i18n_link('resources')  => trans('shin::fields.resources'), ],
+
      'breadcrumbs' => [
         i18n_link('/')  => trans('shin::fields.home'),
         i18n_link('/bibles')   => trans('shin::fields.bibles'),
         i18n_link('/languages/[ $bible->iso ]')   => $bible->iso,
-            '#'         =>  $bible->title,
     ],
 
 ])
