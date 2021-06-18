f51d21a7f31c6a7b61f94aaef5ee63e660b145da
Ken Bitgood <ken@dbs.org>
2021-06-18 14:13:15 -0500

    Suggest Bibles Page Update

--- resources/views/about/add.blade.php
+++ resources/views/about/add.blade.php
@@ -7,7 +7,10 @@
         a.nav-about     {color: var(--primary-color)!important}
         .about-content  {margin: 1rem auto; padding:1em 15%;}
         .about          {margin-bottom:1.5em;}
-        h4              {text-align:left;}
+        h4              {text-align:left;margin: .5em 0;}
+        .aside p         {font-size: .8em;margin-top: 1em}
+    .aside img          {margin-right: 1em; float:left; width: 80px;}
+    .freebirdFormviewerViewCenteredContent {width:100%!important;}
     </style>
 @endsection
 
@@ -16,8 +19,8 @@
 @endsection
 
 @include('shin::_partials.banner', [
-'title'           => trans('shin::fab.about.title'),
-'subtitle'        => trans('shin::fab.about.subtitle'),
+'title'           => trans('shin::fab.about.suggest.title'),
+'subtitle'        => trans('shin::fab.about.suggest.subtitle'),
 'icon'            => 'people_agencies',
 'iconClass'       => 'banner-icon',
 'iconType'        => 'icons',
@@ -38,9 +41,18 @@ i18n_link('/about/contact') => trans('shin::fields.contact'),
 @section('main')
 
     <div class="row">
-        <div class="medium-3">Help</div>
-        <div class="medium-9">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSclHomVT1Ad7RwjaNG-FgS3N0iDph7qIzozHcRtif1C4HJMYQ/viewform?embedded=true" width="100%" height="1381" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <div class="medium-3 aside">
+            <h4>{{ trans('shin::fab.about.suggest.heading') }}</h4>
+            <hr />
+            <img src="https://images.bible.cloud/fab/fab_color_helping.svg" alt="Suggest Bibles" />
+            <p>{!! trans('shin::fab.about.suggest.instructions') !!}</p>
+            <p><a style="text-align: right" href="https://images.bible.cloud/fab/docs/FAB_Suggest_Bibles.zip">Download Sheet</a></p>
         </div>
-    </div>
+        <div class="medium-9">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSclHomVT1Ad7RwjaNG-FgS3N0iDph7qIzozHcRtif1C4HJMYQ/viewform?embedded=true"
+                    width="100%" height="1381" frameborder="0" marginheight="0" marginwidth="0">
+                Loading…</iframe>
+        </div>
+    </d
+
 @endsection
